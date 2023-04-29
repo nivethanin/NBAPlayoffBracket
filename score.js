d72a5dc7dbe2d3c107c7b697999051f5ea257223
nivethanin <nivethanin@gmail.com>
2023-04-29 02:28:48 -0400

    .

--- score.js
+++ score.js
@@ -1,7 +1,7 @@
 let MAX_NUM_TEAMS = 13
 
-let CORRECTTEAMS = ["ES8", "NYK", "PHI","BOS","DEN","PHO"]
-let CORRECTSERIES = [5, 5, 4, 6, 5, 5]
+let CORRECTTEAMS = ["ES8", "NYK", "PHI","BOS","DEN","PHO","","LAL"]
+let CORRECTSERIES = [5, 5, 4, 6, 5, 5, 7, 6]
 
 
 function generateScore(){
